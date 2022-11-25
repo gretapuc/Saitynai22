770f2677021c675824a63f03d33d6ad6c588c1a3
gretapuc <gretapucilauskaite@gmail.com>
2022-11-25 08:59:32 +0200

    Entities fix

--- Saitynai/Saitynai/Data/Entities/Competition.cs
+++ Saitynai/Saitynai/Data/Entities/Competition.cs
@@ -3,7 +3,7 @@ using System.ComponentModel.DataAnnotations;
 
 namespace Saitynai.Data.Entities
 {
-    public class Competition
+    public class Competition : IUserOwnedResource
     {
         public int Id { get; set; }
         public string Name { get; set; }
